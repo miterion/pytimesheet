4a2708323ba3b520003958dede13c83042a6db23
Heiko Carrasco <heiko.carrasco@yahoo.com>
2017-06-26 23:37:34 +0200

    Added requierements to setup.py

--- setup.py
+++ setup.py
@@ -2,6 +2,9 @@ from setuptools import setup, find_packages
 
 setup(
         name='timetrack',
+        version='0.0.1',
+        license='GPL',
+        install_requires =['Jinja2'],
         packages=find_packages(),
         include_package_data=True,
         entry_points={
