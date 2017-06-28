f7cf9f8f56aae673ed8e3e0f65722f8115fb8a0e
Heiko Carrasco <heiko.carrasco@yahoo.com>
2017-06-28 11:14:02 +0200

    Fixed error due to wrong name

--- setup.py
+++ setup.py
@@ -2,14 +2,14 @@ from setuptools import setup, find_packages
 
 setup(
         name='pytimesheet',
-        version='0.0.1',
+        version='0.0.2',
         license='GPL',
         install_requires =['Jinja2'],
         packages=find_packages(),
         include_package_data=True,
         entry_points={
             'console_scripts': [
-                'pytimetrack=pytimetrack.cli:main',
+                'timetrack=pytimesheet.cli:main',
             ],
         }
 )
