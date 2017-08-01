956091256e47c485cc799f7d6976b0ed40af93a4
Heiko Carrasco <heiko.carrasco@yahoo.com>
2017-08-01 12:08:17 +0200

    Moved template files into module

--- setup.py
+++ setup.py
@@ -2,12 +2,13 @@ from setuptools import setup, find_packages
 
 setup(
         name='pytimesheet',
-        version='0.0.6',
+        version='0.0.8',
         license='GPL',
         description='timetracking for hiwis at tu darmstadt',
         install_requires =['Jinja2'],
         packages=find_packages(),
         include_package_data=True,
+        package_data = {'pytimesheet': ['template/*']},
         entry_points={
             'console_scripts': [
                 'pytimesheet=pytimesheet.cli:main',
